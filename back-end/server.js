2b57e939041640c59e01c5f06047982c18877cb8
alihasan6336 <mac@Alis-MacBook-Air.local>
2024-10-21 10:10:40 +0300

    return to the original backend

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
 const DB = process.env.DATABASE.replace(
-  "pass123",
+  "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 
